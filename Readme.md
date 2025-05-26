fb16ee02b7cc633c0394a282889ff7146c6ef9b6
Nikhil <nikhilv7x7@gmail.com>
2025-05-26 22:39:25 +0530

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,2 @@
+Zin is a toy programming language. 
+I made in order to learn about ocaml,lexing,ASTs
---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Zin Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+    let 
+      system = "x86_64-linux";
+      pkgs =  import nixpkgs { inherit system; };
+    in
+    {
+
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          ocamlPackages.ocaml
+          ocamlPackages.dune_3
+          ocamlPackages.findlib
+          ocamlPackages.utop
+          ocamlPackages.base
+          ocamlPackages.odoc
+          ocamlformat
+          ocamlPackages.ocaml-lsp
+          ocamlPackages.lsp
+        ];
+      };
+
+  };
+}
