d5c4fc8b540b1da686e47f14891c50eaef091b86
Nikhil <nikhilv7x7@gmail.com>
2025-06-02 11:59:21 +0530

    deploy: 🚀 Nixified the repo

--- flake.nix
+++ flake.nix
@@ -1,35 +1,66 @@
 {
-  description = "Zin Flake";
+  description = "Zin - A cool interpreted language made in OCaml";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
     {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          ocamlPackages.ocaml
-          ocamlPackages.dune_3
-          ocamlPackages.findlib
-          ocamlPackages.utop
-          ocamlPackages.base
-          ocamlPackages.ppx_deriving
-          ocamlPackages.ppxlib
-          ocamlPackages.ounit
-          ocamlPackages.cmdliner
-          ocamlPackages.odoc
-          ocamlformat
-          ocamlPackages.ocaml-lsp
-          ocamlPackages.lsp
-        ];
-      };
+      in
+      {
 
-    };
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            ocamlPackages.ocaml
+            ocamlPackages.dune_3
+            ocamlPackages.findlib
+            ocamlPackages.utop
+            ocamlPackages.base
+            ocamlPackages.ppx_deriving
+            ocamlPackages.ppxlib
+            ocamlPackages.ounit
+            ocamlPackages.cmdliner
+            ocamlPackages.odoc
+            ocamlformat
+            ocamlPackages.ocaml-lsp
+            ocamlPackages.lsp
+          ];
+
+        };
+
+        packages.default = pkgs.ocamlPackages.buildDunePackage {
+          pname = "zin";
+          version = "v1.0.0";
+          meta = {
+            homepage = "https://github.com/nikhilcodes777/zin";
+            description = "Zin - A cool interpreted language made in OCaml";
+            license = pkgs.lib.licenses.mit;
+          };
+          src = self;
+          buildInputs = with pkgs.ocamlPackages; [
+            cmdliner
+            base
+            ppx_deriving
+            ppxlib
+          ];
+
+        };
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/zin";
+        };
+      }
+    );
+
 }
